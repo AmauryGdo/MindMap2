e8e783fa9aa322534081a0f4efde32d262c36c94
AmauryGdo <amaury.giordano@epfedu.fr>
2020-04-10 13:53:41 +0200

    ajout layout cuisine horizontale et modif de l'activity cuisine

--- app/src/main/java/com/example/anais/test/cuisine.java
+++ app/src/main/java/com/example/anais/test/cuisine.java
@@ -1,6 +1,7 @@
 package com.example.anais.test;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,87 +21,98 @@ public class cuisine extends AppCompatActivity {
     private RelativeLayout chat;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView( R.layout.cuisine_vertical);
-        frigo= findViewById(R.id.frig1);
-        frigo.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v){
-                Intent i = new Intent(cuisine.this, Ecrire.class);
-                i.putExtra("objetclique", "frigo"); //Optional parameters
-                startActivity(i);
 
-            }
-        });
+        switch (getResources().getConfiguration().orientation) {
 
-        orange= findViewById(R.id.orange1);
-        orange.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v){
-                Intent i = new Intent(cuisine.this, Ecrire.class);
-                i.putExtra("objetclique", "orange"); //Optional parameters
-                startActivity(i);
-            }
-        });
+            case Configuration.ORIENTATION_PORTRAIT:
 
-        corbeille= findViewById(R.id.corbeille1);
-        corbeille.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v){
-                Intent i = new Intent(cuisine.this, Ecrire.class);
-                i.putExtra("objetclique", "corbeille"); //Optional parameters
-                startActivity(i);
-            }
-        });
-        fenetre= findViewById(R.id.fenetre1);
-        fenetre.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v){
-                Intent i = new Intent(cuisine.this, Ecrire.class);
-                i.putExtra("objetclique", "fenetre"); //Optional parameters
-                startActivity(i);
-            }
-        });
+                setContentView(R.layout.cuisine_vertical);
+                frigo = findViewById(R.id.frig1);
+                frigo.setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(cuisine.this, Ecrire.class);
+                        i.putExtra("objetclique", "frigo"); //Optional parameters
+                        startActivity(i);
 
-        four= findViewById(R.id.four1);
-        four.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v){
-                Intent i = new Intent(cuisine.this, Ecrire.class);
-                i.putExtra("objetclique", "four"); //Optional parameters
-                startActivity(i);
-            }
-        });
+                    }
+                });
 
-        plante= findViewById(R.id.plante1);
-        plante.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v){
-                Intent i = new Intent(cuisine.this, Ecrire.class);
-                i.putExtra("objetclique", "plante"); //Optional parameters
-                startActivity(i);
-            }
-        });
+                orange = findViewById(R.id.orange1);
+                orange.setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(cuisine.this, Ecrire.class);
+                        i.putExtra("objetclique", "orange"); //Optional parameters
+                        startActivity(i);
+                    }
+                });
 
-        armoire= findViewById(R.id.armoire1);
-        armoire.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v){
-                Intent i = new Intent(cuisine.this, Ecrire.class);
-                i.putExtra("objetclique", "armoire"); //Optional parameters
-                startActivity(i);
-            }
-        });
+                corbeille = findViewById(R.id.corbeille1);
+                corbeille.setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(cuisine.this, Ecrire.class);
+                        i.putExtra("objetclique", "corbeille"); //Optional parameters
+                        startActivity(i);
+                    }
+                });
+                fenetre = findViewById(R.id.fenetre1);
+                fenetre.setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(cuisine.this, Ecrire.class);
+                        i.putExtra("objetclique", "fenetre"); //Optional parameters
+                        startActivity(i);
+                    }
+                });
 
-        chat= findViewById(R.id.chat1);
-        chat.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v){
-                Intent i = new Intent(cuisine.this, Ecrire.class);
-                i.putExtra("objetclique", "chat"); //Optional parameters
-                startActivity(i);
-            }
-        });
+                four = findViewById(R.id.four1);
+                four.setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(cuisine.this, Ecrire.class);
+                        i.putExtra("objetclique", "four"); //Optional parameters
+                        startActivity(i);
+                    }
+                });
+
+                plante = findViewById(R.id.plante1);
+                plante.setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(cuisine.this, Ecrire.class);
+                        i.putExtra("objetclique", "plante"); //Optional parameters
+                        startActivity(i);
+                    }
+                });
+
+                armoire = findViewById(R.id.armoire1);
+                armoire.setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(cuisine.this, Ecrire.class);
+                        i.putExtra("objetclique", "armoire"); //Optional parameters
+                        startActivity(i);
+                    }
+                });
+
+                chat = findViewById(R.id.chat1);
+                chat.setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(cuisine.this, Ecrire.class);
+                        i.putExtra("objetclique", "chat"); //Optional parameters
+                        startActivity(i);
+                    }
+                });
+                break;
+
+
+            /*case Configuration.ORIENTATION_LANDSCAPE:
+                setContentView(R.layout.cuisine_horizontal);*/
+        }
     }
 }
