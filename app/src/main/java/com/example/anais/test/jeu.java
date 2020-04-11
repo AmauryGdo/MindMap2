3d3a33ec72eac11eb4f328a37ecb4f92716b67d0
AmauryGdo <amaury.giordano@epfedu.fr>
2020-04-11 12:46:38 +0200

    jeu horizontal

--- app/src/main/java/com/example/anais/test/jeu.java
+++ app/src/main/java/com/example/anais/test/jeu.java
@@ -1,6 +1,7 @@
 package com.example.anais.test;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,66 +9,125 @@ import android.widget.RelativeLayout;
 
 // Il y a beaucoup de répétition dans notre code j'ai donc choisi de commenter Chambre, Main Activité, Choix Pièce et Ecrire
 public class jeu extends AppCompatActivity {
-    //définition des variables
-    private RelativeLayout guitare;
-    private RelativeLayout xylophone;
-    private RelativeLayout tableau;
-    private RelativeLayout pouf;
 
     //Nous sommes dans la salle de jeu et elle contient 4 zones clicables
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.jeu_vertical);
 
-        guitare= findViewById(R.id.guitare);
-        guitare.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-                Intent i = new Intent(jeu.this, Ecrire.class);
-                i.putExtra("objetclique", "guitare"); //Optional parameters
-                startActivity(i);
+        switch (getResources().getConfiguration().orientation) {
 
-            }
-        });
-
-        tableau= findViewById(R.id.tableau);
-        tableau.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-                Intent i = new Intent(jeu.this, Ecrire.class);
-                i.putExtra("objetclique", "tableau"); //Optional parameters
-                startActivity(i);
-
-            }
-        });
+            case Configuration.ORIENTATION_PORTRAIT:
+                setContentView(R.layout.jeu_vertical);
 
 
+                RelativeLayout guitare = findViewById(R.id.guitare);
+                guitare.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(jeu.this, Ecrire.class);
+                        i.putExtra("objetclique", "guitare"); //Optional parameters
+                        startActivity(i);
+
+                    }
+                });
+
+                RelativeLayout tableau = findViewById(R.id.tableau);
+                tableau.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(jeu.this, Ecrire.class);
+                        i.putExtra("objetclique", "tableau"); //Optional parameters
+                        startActivity(i);
+
+                    }
+                });
 
 
-        xylophone= findViewById(R.id.table);
-        xylophone.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-                Intent i = new Intent(jeu.this, Ecrire.class);
-                i.putExtra("objetclique", "xylophone"); //Optional parameters
-                startActivity(i);
+                RelativeLayout xylophone = findViewById(R.id.table);
+                xylophone.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(jeu.this, Ecrire.class);
+                        i.putExtra("objetclique", "xylophone"); //Optional parameters
+                        startActivity(i);
 
-            }
-        });
+                    }
+                });
 
-        pouf= findViewById(R.id.pouf);
-        pouf.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-                Intent i = new Intent(jeu.this, Ecrire.class);
-                i.putExtra("objetclique", "pouf"); //Optional parameters
-                startActivity(i);
+                RelativeLayout pouf = findViewById(R.id.pouf);
+                pouf.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(jeu.this, Ecrire.class);
+                        i.putExtra("objetclique", "pouf"); //Optional parameters
+                        startActivity(i);
 
-            }
-        });
+                    }
+                });
+                break;
 
+
+            case Configuration.ORIENTATION_LANDSCAPE:
+                setContentView(R.layout.jeu_horizontal);
+
+                guitare = findViewById(R.id.guitare);
+                guitare.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(jeu.this, Ecrire.class);
+                        i.putExtra("objetclique", "guitare"); //Optional parameters
+                        startActivity(i);
+
+                    }
+                });
+
+                tableau = findViewById(R.id.tableau);
+                tableau.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(jeu.this, Ecrire.class);
+                        i.putExtra("objetclique", "tableau"); //Optional parameters
+                        startActivity(i);
+
+                    }
+                });
+
+
+                xylophone = findViewById(R.id.table);
+                xylophone.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(jeu.this, Ecrire.class);
+                        i.putExtra("objetclique", "xylophone"); //Optional parameters
+                        startActivity(i);
+
+                    }
+                });
+
+                pouf = findViewById(R.id.pouf);
+                pouf.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(jeu.this, Ecrire.class);
+                        i.putExtra("objetclique", "pouf"); //Optional parameters
+                        startActivity(i);
+
+                    }
+                });
+        }
     }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
 
+        // Checks the orientation of the screen
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            setContentView(R.layout.jeu_horizontal);
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            setContentView(R.layout.jeu_vertical  );
+
+        }
+    }
 }
